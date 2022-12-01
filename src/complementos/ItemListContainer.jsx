6b856c8c6f36b2af1f589a2aa6c5f5cdecdddc94
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2022-11-30 23:45:48 -0300

    Update ItemListContainer.jsx

--- src/complementos/ItemListContainer.jsx
+++ src/complementos/ItemListContainer.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const ItemListContainer = (greeting) => {
+const ItemListContainer = ({greeting}) => {
     return(
         <div className="container py-5">
             <div className="row">
                 <div className="col-md-12">
-                  <p>{}</p>
+                  <p>{greeting}</p>
                 </div>
             </div>
         </div>
