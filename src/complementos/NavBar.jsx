1842d4e5a21df20d1ff378c92c0042e4e6094247
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2022-12-18 09:04:55 -0300

    Update NavBar.jsx

--- src/complementos/NavBar.jsx
+++ src/complementos/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CartWidget from "./CartWidget";
+import {Link, NavLink} from "react-router-dom";
 
 const NavBar = () => {
     return (
@@ -7,23 +8,26 @@ const NavBar = () => {
             <div className="col-md-12">
             <nav class="navbar navbar-expand-lg bg-light">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#"><a className="navbar-brand" href="#"><img src={"images/logopizza.png"} alt={"logo de pizza"} width={120}></img></a></a>
+                    <Link class="navbar-brand" href="#"><a className="navbar-brand" href="/"><img src={"images/logopizza.png"} alt={"logo de pizza"} width={120}></img></a></Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                        <NavLink className="nav-link" activeclassNamename="page" to={"/"}>Inicio</NavLink>
                         </li>
                         <li class="nav-item">
-                        <a class="nav-link" href="#">Tiendas</a>
+                        <NavLink className="nav-link" activeclassname="page" to={"/categoria/Pizzas"}>Pizzas</NavLink>
                         </li>
                         <li class="nav-item">
-                        <a class="nav-link" href="#">Pizzas</a>
+                        <NavLink className="nav-link" activeclassname="page" to={"/categoria/Hamburguesas"}>Hamburguesas</NavLink>
                         </li>
                         <li class="nav-item">
-                        <a class="nav-link disabled">Contacto</a>
+                        <NavLink className="nav-link"activeclassname="page" to={""}>Locales</NavLink>
+                        </li>
+                        <li class="nav-item">
+                        <NavLink className="nav-link" activeclassname="page" to={""}>Contacto</NavLink>
                         </li>
                     </ul>
                     </div>
