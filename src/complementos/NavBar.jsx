ba3e2d376380811f5115020e4175d46bda36dca4
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-01-22 22:12:51 -0300

    Update NavBar.jsx

--- src/complementos/NavBar.jsx
+++ src/complementos/NavBar.jsx
@@ -6,27 +6,27 @@ const NavBar = () => {
     return (
        <div className="row">
             <div className="col-md-12">
-            <nav class="navbar navbar-expand-lg bg-light">
-                <div class="container-fluid">
-                    <Link class="navbar-brand" to="#"><img src={"images/logopizza.png"} alt={"logo de pizza"} width={120}></img></Link>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="#"><img src={"images/logopizza.png"} alt={"logo de pizza"} width={120}></img></Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                        <NavLink className="nav-link" activeclassNamename="page" to={"/"}>Inicio</NavLink>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                        <NavLink className="nav-link" activeclassname="active" to={"/"}>Inicio</NavLink>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item">
                         <NavLink className="nav-link" activeclassname="page" to={"/categoria/Pizzas"}>Pizzas</NavLink>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item">
                         <NavLink className="nav-link" activeclassname="page" to={"/categoria/Hamburguesas"}>Hamburguesas</NavLink>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item">
                         <NavLink className="nav-link"activeclassname="page" to={""}>Locales</NavLink>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item">
                         <NavLink className="nav-link" activeclassname="page" to={""}>Contacto</NavLink>
                         </li>
                     </ul>
