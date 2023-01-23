6e62fdf5c28a0fcbfd5d1fe19090f2f8ded57c28
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-01-22 22:12:35 -0300

    Update CartWidget.jsx

--- src/complementos/CartWidget.jsx
+++ src/complementos/CartWidget.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { CartContext } from "./context/CartContext";
 
 const CartWidget = () => {
-    return(
-        <button type="button" class="btn bg-light position-relative">
-            <img src={"images/bag.svg"} alt={"Bolsa"} width={32} />
-        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">5</span>
-        </button>
-    )
+    const {cartTotals} = useContext(CartContext)
+
+    if (cartTotals()===0) {
+        return("")
+    }
+
+    return cartTotals() ? <Link to={"/cart"} type="button" className="btn bg-light position-relative">
+            <img src={"images/bag.svg"} alt={"Carro"} width={32} />
+        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{cartTotals()}</span>
+        </Link> :"";
+    
 }
 
 export default CartWidget;
