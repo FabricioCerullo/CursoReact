66a80367ca3d155f241948e3b0dcb88c42a87c27
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-01-22 22:12:53 -0300

    Update Producto.jsx

--- src/complementos/Producto.jsx
+++ src/complementos/Producto.jsx
@@ -1,33 +1,30 @@
-import React, {useState}from "react";
+import React, {useContext,useState}from "react";
 import listaProd from "./json/listaProd.json";
 import { useEffect } from "react";
 import {useParams} from "react-router-dom";
+import ItemCount from "./ItemCount";
+import { CartContext } from "./context/CartContext";
 
 
-const Producto = () => {
-    const [item, setItem] = useState({});
+const Producto = ({item}) => {
     const {id} = useParams();
+    const {addItem} = useContext(CartContext);
+    const [stockItem, setStockItem] = useState(0); 
 
-    useEffect(()=>{
-        const promesa  = new Promise((resolve)=>{
-            setTimeout(()=>{
-                resolve(listaProd.find(item => item.id === id));
-            });
-        })
-        promesa.then(data => {
-            setItem(data);
-        })
-    }, [id])
+    const onAdd = (quantity) =>{
+        addItem(item, quantity);
+    }
 
     return (
         <div className="container">
             <div className = "row">
                 <div className="col-md-6 offset-md-3 text-center">
-                        <img src={item.imagen} className={"img-fluid"} alt={item.nombre} />
-                        <h1>{item.nombre}</h1>
-                        <p>{item.descripcion}</p>
-                        <p><b>${item.precio}</b></p>
-                        <p className="alert alert-success">{item.stock}</p>
+                        <img src={item.Imagen} className={"img-fluid"} alt={item.Nombre} />
+                        <h1>{item.Nombre}</h1>
+                        <p>{item.Descripcion}</p>
+                        <p><b>${item.Precio}</b></p>
+                        <p className="alert alert-success">{item.Stock}</p>
+                        <ItemCount onAdd={onAdd} />
                 </div>
             </div>
         </div>
